9bcd4bfa612ea630c305ef536ced5bacb775ed3e
Greg Sienkiewicz <greg.sienkiewicz@rewind.io>
2023-10-11 10:08:54 -0400

    init the AWS terraform provider

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.20.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  description = "The region to deploy into (defaults to 'ca-central-1' for legal reasons)"
+  type        = string
+
+  default = "ca-central-1"
+}
