d718bd9e98870ccbe386cc0923f36c4af813727a
Greg Sienkiewicz <greg.sienkiewicz@rewind.io>
2023-10-11 11:07:35 -0400

    add default security group for use in the CTF vpc

---
+++ sg.tf
@@ -0,0 +1,34 @@
+###======================== CTF Security Groups ====================== ###
+
+resource "aws_security_group" "ctf" {
+  name        = "CTF Default Security Group"
+  description = "CTF Default Security Group"
+  vpc_id      = aws_vpc.ctf.id
+
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-default-ctf"
+  }
+}
